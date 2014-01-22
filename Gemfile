f6867e33b8180b560368209e9122407a40cc6ebe
ivan <steel5rat@gmail.com>
2014-01-22 20:32:32 +0300

    blog and feedback services added

--- Gemfile
+++ Gemfile
@@ -41,6 +41,8 @@ gem 'jquery-rails'
 
 # Refinery CMS
 gem 'refinerycms', '~> 2.1.0'
+gem 'refinerycms-blog', '~> 2.1.0'
+gem 'refinerycms-inquiries', '~> 2.1.0'
 
 # Optionally, specify additional Refinery CMS Extensions here:
 gem 'refinerycms-acts-as-indexed', '~> 1.0.0'
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,9 @@ Refinery::Pages::Engine.load_seed
 
 # Added by Refinery CMS Feeds extension
 Refinery::Feeds::Engine.load_seed
+
+# Added by Refinery CMS Blog engine
+Refinery::Blog::Engine.load_seed
+
+# Added by Refinery CMS Inquiries engine
+Refinery::Inquiries::Engine.load_seed
